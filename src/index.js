c8f10a0c30061a706c10affeb975e639d43deb19
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2019-08-19 09:15:37 +0300

    Handle unexpected Mongo errors (#9)
    Catch errors from Mongo which are not caught by Agenda. Should make the application exit on ECONNRESET-errors.

--- src/index.js
+++ src/index.js
@@ -51,6 +51,11 @@ async function run() {
 	const Logger = createLogger();
 	const Mongo = await MongoClient.connect(MONGO_URI, {useNewUrlParser: true});
 
+	Mongo.on('error', err => {
+		Logger.log('error', 'Error stack' in err ? err.stack : err);
+		process.exit(1);
+	});
+
 	process
 		.on('SIGINT', handleExit)
 		.on('unhandledRejection', handleExit)
