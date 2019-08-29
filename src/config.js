bcc6bd068abc984f94686a6f964b228db58d3dfe
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2019-08-29 11:01:47 +0300

    Use different credentials for tasks  (#11)
    Use different credentials for the tasks and the controller. Resolves #4.

--- src/config.js
+++ src/config.js
@@ -36,6 +36,10 @@ export const TZ = readEnvironmentVariable('TZ', {defaultValue: ''});
 export const API_URL = readEnvironmentVariable('API_URL');
 export const API_USERNAME = readEnvironmentVariable('API_USERNAME');
 export const API_PASSWORD = readEnvironmentVariable('API_PASSWORD');
+export const API_USERNAME_IMPORTER = readEnvironmentVariable('API_USERNAME_IMPORTER');
+export const API_PASSWORD_IMPORTER = readEnvironmentVariable('API_PASSWORD_IMPORTER');
+export const API_USERNAME_TRANSFORMER = readEnvironmentVariable('API_USERNAME_TRANSFORMER');
+export const API_PASSWORD_TRANSFORMER = readEnvironmentVariable('API_PASSWORD_TRANSFORMER');
 
 export const MONGO_URI = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1/db'});
 export const AMQP_URL = readEnvironmentVariable('AMQP_URL', {defaultValue: 'amqp://127.0.0.1:5672'});
@@ -95,8 +99,8 @@ export const CONTAINER_TEMPLATE_TRANSFORMER = {
 	Env: [
 		`AMQP_URL=${AMQP_URL}`,
 		`API_URL=${API_URL}`,
-		`API_USERNAME=${API_USERNAME}`,
-		`API_PASSWORD=${API_PASSWORD}`,
+		`API_USERNAME=${API_USERNAME_TRANSFORMER}`,
+		`API_PASSWORD=${API_PASSWORD_TRANSFORMER}`,
 		'ABORT_ON_INVALID_RECORDS=false',
 		`DEBUG=${process.env.DEBUG}`
 	],
@@ -117,8 +121,8 @@ export const CONTAINER_TEMPLATE_IMPORTER = {
 	Env: [
 		`AMQP_URL=${AMQP_URL}`,
 		`API_URL=${API_URL}`,
-		`API_USERNAME=${API_USERNAME}`,
-		`API_PASSWORD=${API_PASSWORD}`,
+		`API_USERNAME=${API_USERNAME_IMPORTER}`,
+		`API_PASSWORD=${API_PASSWORD_IMPORTER}`,
 		`DEBUG=${process.env.DEBUG}`
 	],
 	Healthcheck: {
