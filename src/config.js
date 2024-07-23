01d8649456c1438d8e014dc4cf7bc29963fde985
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2024-07-23 14:33:02 +0300

    fix mongo uri env variable

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import {readEnvironmentVariable} from '@natlibfi/melinda-backend-commons';
 
-export const mongoUri = readEnvironmentVariable('MONGO_URI');
+export const mongoUri = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1:27017/db'});
 export const mongoDatabaseAndCollections = readEnvironmentVariable('MONGO_DATABASE_AND_COLLECTIONS', {format: v => JSON.parse(v)});
-export const pollTime = readEnvironmentVariable('POLL_TIME', {defaultValue: 21600000}); // 2h in ms (1000 ms / 60 sec / 60 min)
+export const pollTime = readEnvironmentVariable('POLL_TIME', {defaultValue: 21600000}); // 6h in ms (1000 ms / 60 sec / 60 min)
 
 export const testMoment = '2021-05-08'; // Date for testing
 export const earliestMoment = '2000-01-01'; // Earliest date for filtering
