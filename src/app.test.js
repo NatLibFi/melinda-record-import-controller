0840dd79c274c1bd141affd7f4dc1c991ca11535
Minttu Hurme <ammsalme@users.noreply.github.com>
2026-02-02 15:07:36 +0200

    Node 22 & some updates ; Dependency & Maintenance update (#177) (#176)
    * Feature node 22 (#168)
    
    * init node 22 native
    * update-actions
    * update-dockerfile
    * feature amqp cleanup
    * init node 22 native and implemented rabbit queue check
    * improve logging
    * improve logging
    
    * Update deps
    
    * 2.0.0-alpha.1
    
    * Dependency & Maintenance update (#177)
    
    * Bump js-yaml from 4.1.0 to 4.1.1 (#175)
    
    * Bump nodemailer and @natlibfi/melinda-backend-commons (#174)
    Updates `nodemailer` from 6.9.15 to 7.0.13
    Updates `@natlibfi/melinda-backend-commons` from 2.3.2 to 2.3.9
    
    * Bump lodash from 4.17.21 to 4.17.23 (#173)
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.21 to 4.17.23.
    
    * Bump the development-dependencies group across 1 directory with 7 updates (#172)
    
    * Bump the development-dependencies group across 1 directory with 7 updates
    
    | Package | From | To |
    | --- | --- | --- |
    | [@natlibfi/fixugen](https://github.com/NatLibFi/fixugen-js) | `3.0.0-alpha.6` | `3.0.0` |
    | [@natlibfi/fixura-mongo](https://github.com/NatLibFi/fixura-mongo-js) | `3.0.0-alpha.5` | `3.0.2` |
    | [@onify/fake-amqplib](https://github.com/onify/fake-amqplib) | `3.2.0` | `3.4.0` |
    | [cross-env](https://github.com/kentcdodds/cross-env) | `10.0.0` | `10.1.0` |
    | [esbuild](https://github.com/evanw/esbuild) | `0.25.9` | `0.25.12` |
    | [eslint](https://github.com/eslint/eslint) | `9.35.0` | `9.39.2` |
    
    * Bump actions/checkout from 5 to 6 (#169)
    
    * Bump actions/setup-node from 5 to 6 (#170)
    
    * Bump the production-dependencies group across 1 directory with 3 updates (#171)
    Updates `@natlibfi/melinda-backend-commons` from 3.0.0-alpha.6 to 3.0.4
    Updates `@natlibfi/melinda-record-import-commons` from 13.0.0-alpha.5 to 13.0.2
    Updates `mongodb` from 6.19.0 to 6.21.0
    
    * Update deps
    
    * Update copyright year
    
    * 2.0.1-alpha.1
    
    ---------
    
    Co-authored-by: Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>

---
+++ src/app.test.js
@@ -0,0 +1,102 @@
+import assert from 'node:assert';
+import * as fakeAmqpLib from '@onify/fake-amqplib';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+import mongoFixturesFactory from '@natlibfi/fixura-mongo';
+import {createAmqpOperator} from '@natlibfi/melinda-record-import-commons';
+
+import {startApp} from './app.js';
+import {testMoment} from './config.js';
+
+let mongoFixtures;
+
+generateTests({
+  callback,
+  path: [import.meta.dirname, '..', 'test-fixtures', 'clean'],
+  recurse: false,
+  useMetadataFile: true,
+  fixura: {
+    failWhenNotFound: true,
+    reader: READERS.JSON
+  },
+  hooks: {
+    before: async () => {
+      await initMongofixtures();
+    },
+    beforeEach: () => mongoFixtures.clear(),
+    afterEach: () => mongoFixtures.clear(),
+    after: async () => {
+      await mongoFixtures.close();
+    }
+  }
+});
+
+async function initMongofixtures() {
+  mongoFixtures = await mongoFixturesFactory({
+    rootPath: [import.meta.dirname, '..', 'test-fixtures', 'clean'],
+    gridFS: {bucketName: 'blobmetadatas'},
+    useObjectId: true
+  });
+}
+
+async function callback({
+  getFixture,
+  mongoDatabaseAndCollections,
+  prepareQueueConfigs = [],
+  expectedToFail = false,
+  expectedErrorMsg = ''
+}) {
+  const amqpOperator = await createAmqpOperator(fakeAmqpLib, 'amqp://example.com');
+  try {
+    await prepareQueues(amqpOperator, prepareQueueConfigs);
+    const mongoUrl = await mongoFixtures.getUri();
+    const amqpUrl = 'amqp://example.com';
+    await mongoFixtures.populate(getFixture('dbContents.json'));
+    await startApp({mongoUrl, amqpUrl, webhookUrl: 'test', mongoDatabaseAndCollections}, fakeAmqpLib, testMoment);
+    const dump = await mongoFixtures.dump();
+    const expectedResult = await getFixture('expectedResult.json');
+    assert.deepStrictEqual(dump, expectedResult);
+    assert.equal(expectedToFail, false, 'This is ment to fail!');
+  } catch (error) {
+    if (expectedToFail) {
+      assert.equal(error.message, expectedErrorMsg);
+
+    }
+    assert.equal(expectedToFail, true, 'This is ment to not fail!');
+    throw error;
+  } finally {
+    await amqpOperator.closeChannel();
+    await amqpOperator.closeConnection();
+  }
+}
+
+async function prepareQueues(amqpOperator, prepareQueueConfigs) {
+  const [config, ...rest] = prepareQueueConfigs;
+
+  if (config === undefined) {
+    return;
+  }
+
+  const {queue, recordsToQueue} = config;
+  const records = prepareRecords(recordsToQueue);
+  await sendRecordsToQueue(queue, records);
+  // const count = await amqpOperator.countQueue({blobId, status});
+  // console.log(`${count} records prepared for test`); // eslint-disable-line
+  return prepareQueues(amqpOperator, rest);
+
+  async function sendRecordsToQueue(queue, records) {
+    const [record, ...rest] = records;
+    if (record === undefined) {
+      return;
+    }
+    const {blobId, status} = queue;
+    await amqpOperator.sendToQueue({blobId, status, headers: {test: true}, data: record});
+    return sendRecordsToQueue(queue, rest);
+  }
+
+  function prepareRecords(amount) {
+    return new Array(amount)
+      .fill({})
+      .map((obj, index) => ({'leader': '02518cam a2200745zi 4500', 'fields': [{'tag': '001', 'value': `${index + 1}`.padStart(9, '0')}]}));
+  }
+}
